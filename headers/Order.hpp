4a9a4e7372d2405e4adb51bda0c8586e58f76aa4
rob12432 <rr555@exeter.ac.uk>
2025-07-15 01:30:53 +0100

    created order cpp file

--- headers/Order.hpp
+++ headers/Order.hpp
@@ -15,4 +15,21 @@ private:
     int quantity;
     std::chrono::high_resolution_clock::time_point timestamp;
 
+
+public:
+    Order(int orderID, orderSide side, OrderType type, double price, int quantity);
+
+
+    int getOrderID() const;
+    orderSide getSide() const;
+    OrderType getType() const;
+    double getPrice() const;
+    int getQuantity() const;
+    std::chrono::high_resolution_clock::time_point getTimestamp() const;
+
+    void setPrice(double newPrice);
+    void setQuantity(int newQuantity);
+    void setTimestamp(std::chrono::high_resolution_clock::time_point newTimestamp);
+
+
 };
--- src/Order.cpp
+++ src/Order.cpp
@@ -1,7 +1,38 @@
-class Order {
+#include "Order.hpp"
 
+Order::Order(int orderID, orderSide side, OrderType type, double price, int quantity):
+    orderID(orderID),side(side),type(type),price(price),quantity(quantity),  timestamp(std::chrono::high_resolution_clock::now()){}
 
+int Order::getOrderID() const {
+    return orderID;
+}
 
+orderSide Order::getSide() const {
+    return side;
+}
 
+OrderType Order::getType() const {
+    return type;
+}
 
-};
+double Order::getPrice() const {
+    return price;
+}
+
+int Order::getQuantity() const {
+    return quantity;
+}
+
+std::chrono::high_resolution_clock::time_point Order::getTimestamp() const {
+    return timestamp;
+}
+
+void Order::setPrice(double newPrice) {
+    price = newPrice;
+}
+void Order::setQuantity(int newQuantity) {
+    quantity = newQuantity;
+}
+void Order::setTimestamp(std::chrono::high_resolution_clock::time_point newTimestamp) {
+    timestamp = newTimestamp;
+}
