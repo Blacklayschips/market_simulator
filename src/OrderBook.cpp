5bb986429ca69cb6dd8fbd27319fc7f726529de3
rob12432 <rr555@exeter.ac.uk>
2025-07-29 14:02:16 +0100

    Coding orderbook

--- src/OrderBook.cpp
+++ src/OrderBook.cpp
@@ -5,10 +5,37 @@
 #include <map>
 
 
-void OrderBook::matchOrder(const Order& orderToMatch) {
+void OrderBook::matchOrder(Order& orderToMatch) {
 
     if (orderToMatch.getSide()==orderSide::BUY) {
+        for (auto it = sellOrders.begin(); it != sellOrders.end() && orderToMatch.getQuantity() > 0;) {
 
+            auto& [priceatLevel,sellOrdersAtLevel] = *it;
+
+           if (orderToMatch.getPrice()>=priceatLevel) {
+
+
+               //iterate through sell orders at this price level
+               for (auto indiSellOrder =sellOrdersAtLevel.begin(); indiSellOrder != sellOrdersAtLevel.end() && orderToMatch.getQuantity() > 0;) {
+
+                   int tradedQuantity = 0;
+                   if (orderToMatch.getQuantity()>=indiSellOrder->getQuantity()) {
+                       tradedQuantity = orderToMatch.getQuantity() - indiSellOrder->getQuantity();
+                   }else {
+                       tradedQuantity = indiSellOrder->getQuantity() - orderToMatch.getQuantity();
+                   }
+
+                   orderToMatch.s
+
+
+               }
+
+
+           }
+
+
+
+        }
 
 
 
